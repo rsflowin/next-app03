eb52421506f9fe8dc6c6bf2f98a9c6ee18e42a8c
rsflowin <rsflowin@gmail.com>
2025-03-16 20:15:24 -0700

    blog post 1

---
+++ components/general/Navbar.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+
+export function Navbar() {
+  return (
+    <nav className="py-5 flex items-center justify-between">
+      <div className="flex items-center gap-6">
+        <Link href="/">
+          <h1 className="text-3xl font-semibold">
+            Blog<span className="text-blue-500">Marshal</span>
+          </h1>
+        </Link>
+
+        <div className="hidden sm:flex items-center gap-6">
+          <Link
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+            href="/dashboard"
+          >
+            Dashboard
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center gap-4">
+        <LoginLink className={buttonVariants()}>Sign in</LoginLink>
+        <RegisterLink className={buttonVariants({ variant: "secondary" })}>
+          Sign up
+        </RegisterLink>
+      </div>
+    </nav>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        hostname: 'img.freepik.com',
+        protocol: 'https',
+        port: ''
+      },
+      {
+        hostname: 'cdn.pixabay.com',
+        protocol: 'https',
+        port: ''
+      },
+      {
+        hostname: 'avatars.githubusercontent.com',
+        protocol: 'https',
+        port: ''
+      }
+    ]
+  }
+  
+  
 };
 
 export default nextConfig;
