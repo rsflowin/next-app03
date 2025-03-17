18b3f2b88d2f96a90d1b927f7a0243049f5eb61a
rsflowin <rsflowin@gmail.com>
2025-03-16 20:26:09 -0700

    fix issue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -18,7 +18,7 @@ export default async function DashboardRouter() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  const data = await getData(user.id);
+  const data = await getData(user?.id);
 
   return (
     <div>
